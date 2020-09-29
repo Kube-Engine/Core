ec96b575198addc01dd93fbafebd4004128c4b63
Matthieu Moinvaziri <m.moinvaziri@gmail.com>
2020-09-29 14:11:48 +0200

    Fixed core missing inline in non-template header function

--- Core.hpp
+++ Core.hpp
@@ -15,6 +15,6 @@ namespace kF
 
     namespace Literal
     {
-        std::basic_string<char> operator""_str(const char *data, const std::size_t size) { return std::basic_string<char>{ data, size }; }
+        inline std::basic_string<char> operator""_str(const char *data, const std::size_t size) { return std::basic_string<char>{ data, size }; }
     }
 }
